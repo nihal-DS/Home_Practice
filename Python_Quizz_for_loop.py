cf2d5edf70de9a03d93a3489ef9f5be7fb560362
1ms17me105 <1ms17me105@gmail.com>
2023-01-11 01:10:40 +0530

    Added Python_Quizz_for_loop

---
+++ Python_Quizz_for_loop.py
@@ -0,0 +1,5 @@
+x = [1,2,3,4]
+for x[-1] in x:
+    print(x[-1])
+# Assigning each value of x to x[-1] and then printing it
+# For more info: https://www.anycodings.com/1questions/1638142/looping-through-a-list-with-index-specific-item
